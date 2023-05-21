5358dc0f12a3da4a9a072e3a69cc3b23ca87b427
Babs-Olokode Aliyah <aliyaheniola91@gmail.com>
2023-05-21 09:51:27 +0100

    initial commit

--- book/filter.py
+++ book/filter.py
@@ -1,7 +1,14 @@
 from django_filters import FilterSet
 
 
+class AuthorFilter:
+    pass
+
+
 class AuthorFilter(FilterSet):
+    pass
+
+
 class Meta:
-        model = AuthorFilter
-        fields = ['first_name', 'last_name']
+    model = AuthorFilter
+    fields = ['first_name', 'last_name']
